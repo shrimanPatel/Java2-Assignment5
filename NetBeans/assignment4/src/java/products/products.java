a6e82d499e70b22181ebb21fc4ec93d8e3fea0b3
Shriman <Shriman@shri.gateway.2wire.net>
2015-07-08 19:06:25 -0400

    Added products package and inside that products class

---
+++ NetBeans/assignment4/src/java/products/products.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package products;
+
+/**
+ *
+ * @author Shriman
+ */
+public class products {
+    private int productID;
+    private String name;
+    private String description;
+    private int quantity;
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
