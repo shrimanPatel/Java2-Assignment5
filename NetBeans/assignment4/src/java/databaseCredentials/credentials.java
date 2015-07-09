9585678550eb25e90c1d5105a1361aaeae576c88
Shriman <Shriman@shri.gateway.2wire.net>
2015-07-08 20:02:48 -0400

    Added getConnection() inside the credentials class to establish the connection

--- NetBeans/assignment4/src/java/databaseCredentials/credentials.java
+++ NetBeans/assignment4/src/java/databaseCredentials/credentials.java
@@ -5,10 +5,29 @@
  */
 package databaseCredentials;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Shriman
  */
 public class credentials {
-    
+
+    public static Connection getConnection() {
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String jdbc = "jdbc:mysql://localhost/c0652964";
+            String userName = "root";
+            String pwd = "";
+            con = DriverManager.getConnection(jdbc, userName, pwd);
+            String query = "SELECT * FROM products";
+
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.err.println("Class not found!" +ex.getMessage());
+        }
+        return con;
+    }
 }
